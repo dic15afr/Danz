c38d29bcbb2f84b0b3fd8965f83256a88e1c9f9c
unknown <dic15afr@student.lu.se>
2018-04-17 00:05:07 +0200

    Initial commit

---
+++ app/src/main/java/com/applications/duckle/danz/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.applications.duckle.danz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
