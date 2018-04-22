cbc827ccab96cf90799688ea076051377996ade8
Absurdius <e94doclrd@hotmail.com>
2018-04-22 18:28:44 +0200

    Initual acceleorometer setup

--- app/src/main/java/com/applications/duckle/danz/Test.java
+++ app/src/main/java/com/applications/duckle/danz/Test.java
@@ -1,13 +1,74 @@
 package com.applications.duckle.danz;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class Test extends AppCompatActivity {
+public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+
+
+    private SensorManager mSensorManager;
+    private Sensor acc_sensors;
+    private float [] acc_values;
+    TextView xText, yText, zText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        acc_sensors = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        xText = (TextView) findViewById(R.id.text_x);
+        yText = (TextView) findViewById(R.id.text_y);
+        zText = (TextView) findViewById(R.id.text_z);
+        mSensorManager.registerListener(this, acc_sensors, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        acc_values = sensorEvent.values;
+        xText.setText("X: " + roundPrecision(acc_values[0], 3));
+        yText.setText("Y: " + roundPrecision(acc_values[1], 3));
+        zText.setText("Z: " + roundPrecision(acc_values[2], 3));
+    }
+
+    private String roundPrecision(float num, int deci){
+        String numb = Float.toString(num);
+        int comma = numb.indexOf(".");
+        if( comma > -1){
+            numb = numb.substring(0, comma + deci);
+        }
+        return numb;
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, acc_sensors, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    /**
+    public void switchBack(View view){
+        mSensorManager.unregisterListener(this);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }*/
 }
