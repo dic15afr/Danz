1fb7b4e0615d367359b95dcd90b52ee28969f3ce
Absurdius <e94doclrd@hotmail.com>
2018-04-23 08:49:35 +0200

    Implemented shake detection

--- app/src/main/java/com/applications/duckle/danz/Test.java
+++ app/src/main/java/com/applications/duckle/danz/Test.java
@@ -8,13 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class AccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+public class Test extends AppCompatActivity implements SensorEventListener {
 
 
     private SensorManager mSensorManager;
     private Sensor acc_sensors;
     private float [] acc_values;
-    TextView xText, yText, zText;
+    TextView xText, yText, zText, shakeText;
+    private long lastUpdate;
+    private final int SHAKE_THRESHOLD = 800;
+    float last_x, last_y, last_z;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,12 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         xText = (TextView) findViewById(R.id.text_x);
         yText = (TextView) findViewById(R.id.text_y);
         zText = (TextView) findViewById(R.id.text_z);
+        shakeText = (TextView) findViewById(R.id.text_shake);
         mSensorManager.registerListener(this, acc_sensors, SensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdate = System.currentTimeMillis();
+        last_x = 0;
+        last_y = 0;
+        last_z = 0;
     }
 
 
@@ -36,6 +44,30 @@ public class AccelerometerActivity extends AppCompatActivity implements SensorEv
         xText.setText("X: " + roundPrecision(acc_values[0], 3));
         yText.setText("Y: " + roundPrecision(acc_values[1], 3));
         zText.setText("Z: " + roundPrecision(acc_values[2], 3));
+
+
+        long curTime = System.currentTimeMillis();
+            // only allow one update every 100ms.
+        if ((curTime - lastUpdate) > 100) {
+            long diffTime = (curTime - lastUpdate);
+            lastUpdate = curTime;
+
+            float x = sensorEvent.values[SensorManager.DATA_X];
+            float y = sensorEvent.values[SensorManager.DATA_Y];
+            float z = sensorEvent.values[SensorManager.DATA_Z];
+
+            float speed = Math.abs(x+y+z - last_x - last_y - last_z) / diffTime * 10000;
+
+            if (speed > SHAKE_THRESHOLD) {
+                shakeText.setText("Shaking");
+            } else {
+                shakeText.setText("Not shaking");
+            }
+            last_x = x;
+            last_y = y;
+            last_z = z;
+        }
+
     }
 
     private String roundPrecision(float num, int deci){
