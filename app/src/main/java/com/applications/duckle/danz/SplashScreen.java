f6e3ff82f75a283c85c2479f6f08eecaa0bb9a46
Albin <dic15afr@student.lu.se>
2018-05-22 18:28:43 +0200

    Fixed layout problem found when using pixel in emulator and changes to keep screen from rotating.

--- app/src/main/java/com/applications/duckle/danz/SplashScreen.java
+++ app/src/main/java/com/applications/duckle/danz/SplashScreen.java
@@ -3,6 +3,7 @@ package com.applications.duckle.danz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 //https://www.bignerdranch.com/blog/splash-screens-the-right-way/
 public class SplashScreen extends AppCompatActivity {
@@ -10,7 +11,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
